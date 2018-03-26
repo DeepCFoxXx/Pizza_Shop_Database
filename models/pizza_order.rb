4685b468bf2bb21ade05c820a6722c9ef1f668d3
DeepCFoxXx <learncode23@gmail.com>
2018-03-26 12:31:01 +0100

    delete added

--- models/pizza_order.rb
+++ models/pizza_order.rb
@@ -54,4 +54,11 @@ class PizzaOrder
     SqlRunner.run(sql, values)
   end
 
+  def delete()
+    sql = "DELETE FROM pizza_orders
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
 end
